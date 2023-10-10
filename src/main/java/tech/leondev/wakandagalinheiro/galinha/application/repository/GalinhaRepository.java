e671fac355acf19bcd9fcf2de70068a814339b8c
leo <nascimentoleo899@gmail.com>
2023-10-10 20:25:30 -0300

    implementa alterar galinha na camada repository

--- src/main/java/tech/leondev/wakandagalinheiro/galinha/application/repository/GalinhaRepository.java
+++ src/main/java/tech/leondev/wakandagalinheiro/galinha/application/repository/GalinhaRepository.java
@@ -1,12 +1,13 @@
 package tech.leondev.wakandagalinheiro.galinha.application.repository;
 
+import tech.leondev.wakandagalinheiro.galinha.application.api.GalinhaRequestDTO;
 import tech.leondev.wakandagalinheiro.galinha.domain.Galinha;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface GalinhaRepository {
-    Galinha saveGalinha(Galinha galinha);
+    Galinha salvarGalinha(Galinha galinha);
 
     List<Galinha> listarGalinhas();
 
--- src/main/java/tech/leondev/wakandagalinheiro/galinha/infra/GalinhaInfraRepository.java
+++ src/main/java/tech/leondev/wakandagalinheiro/galinha/infra/GalinhaInfraRepository.java
@@ -3,6 +3,8 @@ package tech.leondev.wakandagalinheiro.galinha.infra;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import tech.leondev.wakandagalinheiro.galinha.application.api.GalinhaRequestDTO;
 import tech.leondev.wakandagalinheiro.galinha.application.repository.GalinhaRepository;
 import tech.leondev.wakandagalinheiro.galinha.domain.Galinha;
 
@@ -15,7 +17,7 @@ import java.util.UUID;
 public class GalinhaInfraRepository implements GalinhaRepository {
     private final GalinhaSpringDataJpaRepository galinhaSpringDataJpaRepository;
     @Override
-    public Galinha saveGalinha(Galinha galinha) {
+    public Galinha salvarGalinha(Galinha galinha) {
         log.info("[start] GalinhaInfraRepository - saveGalinha");
         Galinha novaGalinha = galinhaSpringDataJpaRepository.save(galinha);
         log.info("[end] GalinhaInfraRepository - saveGalinha");
