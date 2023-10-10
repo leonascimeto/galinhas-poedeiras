8b2d1f7e70e485c6d1c481b9bd81d87cc8814293
leo <nascimentoleo899@gmail.com>
2023-10-10 18:44:04 -0300

    mapeia dominio galinha

--- src/main/java/tech/leondev/wakandagalinheiro/galinha/domain/Galinha.java
+++ src/main/java/tech/leondev/wakandagalinheiro/galinha/domain/Galinha.java
@@ -1,4 +1,26 @@
 package tech.leondev.wakandagalinheiro.galinha.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity(name = "galinhas")
+@Table(name = "galinhas")
 public class Galinha {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", unique = true, nullable = false)
+    private UUID idGalinha;
+    @NotBlank
+    private String name;
+    @NotNull
+    private LocalDateTime dataNascimento;
 }
