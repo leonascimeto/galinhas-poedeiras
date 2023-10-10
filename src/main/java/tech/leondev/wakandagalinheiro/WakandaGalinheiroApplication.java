c36efb11a909f775108eb05a00c7421a62890358
leo <nascimentoleo899@gmail.com>
2023-10-09 21:03:26 -0300

    first commit

---
+++ src/main/java/tech/leondev/wakandagalinheiro/WakandaGalinheiroApplication.java
@@ -0,0 +1,13 @@
+package tech.leondev.wakandagalinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaGalinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakandaGalinheiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/leondev/wakandagalinheiro/WakandaGalinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.leondev.wakandagalinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakandaGalinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
