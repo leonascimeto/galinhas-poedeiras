501531e73e3cd16e62c45d999c61c0a7350986b6
leo <nascimentoleo899@gmail.com>
2023-10-11 21:07:06 -0300

    adiciona reposinse status na camada API Galinha

--- src/main/java/tech/leondev/wakandagalinheiro/galinha/application/api/GalinhaAPI.java
+++ src/main/java/tech/leondev/wakandagalinheiro/galinha/application/api/GalinhaAPI.java
@@ -1,6 +1,7 @@
 package tech.leondev.wakandagalinheiro.galinha.application.api;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,17 +11,22 @@ import java.util.UUID;
 @RequestMapping("v1/galinhas")
 public interface GalinhaAPI {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     GalinhaResponseDTO salvarGalinha(@RequestBody @Valid GalinhaRequestDTO galinhaRequestDTO);
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     List<GalinhaResponseDTO> listarGalinhas();
 
     @GetMapping("{idGalinha}")
+    @ResponseStatus(HttpStatus.OK)
     GalinhaResponseDTO buscarGalinhaPeloId(@PathVariable UUID idGalinha);
 
     @PatchMapping("{idGalinha}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void alterarGalinha(@PathVariable UUID idGalinha, @RequestBody @Valid GalinhaRequestDTO galinhaRequestDTO);
 
     @DeleteMapping("{idGalinha}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void deletarGalinha(@PathVariable UUID idGalinha);
 }
